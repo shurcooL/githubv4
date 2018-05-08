2f1beebeca3383d6367f59e5f091dd4fe1c856de
Dmitri Shuralyov <dmitri@shuralyov.com>
2018-05-08 16:47:50 -0400

    Add package example.
    It provides a very simple starting point and a general idea of how
    to use the package, when viewing documentation via godoc.
    
    It demonstrates use of predeclared Go types as documented in the
    previous commit.

---
+++ example_test.go
@@ -0,0 +1,36 @@
+package githubql_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/shurcooL/githubql"
+	"golang.org/x/oauth2"
+)
+
+func Example() {
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+
+	client := githubql.NewClient(httpClient)
+
+	var q struct {
+		Viewer struct {
+			Login     string
+			CreatedAt time.Time
+			AvatarURL string `graphql:"avatarUrl(size: 72)"`
+		}
+	}
+	err := client.Query(context.Background(), &q, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(q.Viewer.Login)
+	fmt.Println(q.Viewer.CreatedAt)
+	fmt.Println(q.Viewer.AvatarURL)
+}
