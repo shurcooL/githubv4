12f224929086bdfcbe258050ce134ab51a82f46f
Max Knee <Max_Knee@comcast.com>
2020-09-12 15:03:40 -0400

    updating link to new docs
    Signed-off-by: Max Knee <Max_Knee@comcast.com>

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 // Package githubv4 is a client library for accessing GitHub
-// GraphQL API v4 (https://developer.github.com/v4/).
+// GraphQL API v4 (https://docs.github.com/en/graphql).
 //
 // If you're looking for a client library for GitHub REST API v3,
 // the recommended package is github.com/google/go-github/github.
