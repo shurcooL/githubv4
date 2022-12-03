70889c5dac0782236878ca54b6c298693e6a5d68
Dmitri Shuralyov <dmitri@shuralyov.com>
2022-12-03 16:20:05 -0500

    remove FundingPlatformIssuehunt constant
    This is the deprecated copy of a renamed enum that was
    kept available for some time for a smoother transition.
    Its deletion date has come.

--- deprecated.go
+++
@@ -1,8 +0,0 @@
-package githubv4
-
-const (
-	// IssueHunt funding platform.
-	//
-	// Deprecated: Use FundingPlatformIssueHunt instead. This will be deleted after 2022-11-19.
-	FundingPlatformIssuehunt = FundingPlatformIssueHunt
-)
